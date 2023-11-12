dea712c617ec77a357c0ab3ecf3b19e394901e43
Rusty <rustam_ibraeyv@list.ru>
2023-11-12 22:32:58 +0600

    SQLite AZI before GitHub

--- run.py
+++ run.py
@@ -1,20 +1,16 @@
 from app import app, socketio
 
-# if __name__ == '__main__':
-#     socketio.run(app, host='0.0.0.0', port=8000)
-
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, host='0.0.0.0', port=5000)
+
+# if __name__ == '__main__':
+#     app.run()
+#
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
 
 # if __name__ == '__main__':
 #     socketio.run(app, host='0.0.0.0', port=8000, debug=True, allow_unsafe_werkzeug=True)
-
-
-
-# from app import app
-#
-# if __name__ == '__main__':
-#     app.run(debug=True, port=8000)
 
 # if __name__ == '__main__':
 #     with app.app_context():
