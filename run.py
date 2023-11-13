9e3fd7b903b99abbfc5b301a8b9ea3b3a20a69d5
rustykz <92858754+RustyKZ@users.noreply.github.com>
2023-11-14 00:02:22 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 from app import app, socketio
 
-# if __name__ == '__main__':
-#     socketio.run(app, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
 
 # if __name__ == '__main__':
 #     app.run()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
 
-if __name__ == '__main__':
-    socketio.run(app, port=5000)
+# if __name__ == '__main__':
+#     socketio.run(app, port=5000)
 
 # if __name__ == '__main__':
 #     with app.app_context():
