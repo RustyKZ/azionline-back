ac876861ddfdee999909f52206ae4cafa548a037
Rusty <rustam_ibraeyv@list.ru>
2023-11-17 15:04:12 +0600

    SQLite AZI - SSL

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from app import app, socketio
+from OpenSSL import SSL
+context = SSL.Context(SSL.SSLv23_METHOD)
+context.use_privatekey_file('ssl/privkey.pem')
+context.use_certificate_file('ssl/cert.pem')
 
 # if __name__ == '__main__':
 #     socketio.run(app, host='0.0.0.0', port=5000)
@@ -13,7 +17,7 @@ from app import app, socketio
 #     socketio.run(app, port=5000)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, ssl_context=('ssl/cert.pem', 'ssl/privkey.pem'))
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, ssl_context=context)
 
 # if __name__ == '__main__':
 #     with app.app_context():
