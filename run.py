2ac5adc056263be8baba539e2cd5019c33d9786a
Rusty <rustam_ibraeyv@list.ru>
2023-11-18 19:23:56 +0600

    SQLite AZI - SSL2

--- run.py
+++ run.py
@@ -17,7 +17,14 @@ context.use_certificate_file('ssl/cert.pem')
 #     socketio.run(app, port=5000)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, ssl_context=context)
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=80,
+        debug=True,
+        certfile='ssl/cert.pem',
+        keyfile='ssl/privkey.pem',
+        server_side=True)
 
 # if __name__ == '__main__':
 #     with app.app_context():
