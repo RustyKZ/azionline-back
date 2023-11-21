cd3d2514cdad759a65cada8a79351f480756ce25
Rusty <rustam_ibraeyv@list.ru>
2023-11-21 15:51:16 +0600

    SQLite AZI - SSL2

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'db', 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # CLIENT_URL = 'https://localhost:8080'
